a7558381388ebc5f2e24f21211b0002af5188aaa
Jan-Emig <jan.emig@icloud.com>
2022-09-02 23:48:04 +0200

    Fixed No Init Request Bug

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -315,7 +315,8 @@ const App: FC<AppProps> = ({ lang, setLang }) => {
         let timerange_cookie = Cookies.get('tr_tab');
         if (!timerange_cookie || (timerange_cookie !== 'd' && timerange_cookie !== 'w' && timerange_cookie !== 'm' && timerange_cookie !== 'y')) timerange_cookie = 'd';
 
-        window.addEventListener('load', () => getData(timerange_cookie as TabValues));
+        getData(timerange_cookie as TabValues)
+        // window.addEventListener('pageshow', () => getData(timerange_cookie as TabValues));
         setSelectedTab(timerange_cookie as TabValues);
     }, [])
 
@@ -341,7 +342,9 @@ const App: FC<AppProps> = ({ lang, setLang }) => {
     }
 
     const getData = (selected_timerange?: TabValues):void => {
+        console.log('?');
         if (active_request) return;
+        console.log('!');
         active_request = true;
         axios.get('/api/data', { params: { timerange: selected_timerange ?? selectedTab }, headers: { 'Content-Type': 'application/json', 'Accept': 'application/json'}})
         .then((res: AxiosResponse) => {
